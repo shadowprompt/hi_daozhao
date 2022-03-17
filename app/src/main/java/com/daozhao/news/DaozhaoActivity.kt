da990895e7ae0cfae83132cc40686430b72d730c
shadowprompt <huixiong.cn@gmail.com>
2022-03-17 22:44:07 +0800

    [fix]改用默认导航布局

---
+++ app/src/main/java/com/daozhao/news/DaozhaoActivity.kt
@@ -0,0 +1,93 @@
+package com.daozhao.news
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.daozhao.news.databinding.ActivityDaozhaoBinding
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.common.ApiException
+
+class DaozhaoActivity : AppCompatActivity() {
+
+    private var receiver: MyReceiver? = null
+
+    private lateinit var binding: ActivityDaozhaoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDaozhaoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_daozhao)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_discovery
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    inner class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val bundle = intent?.extras
+            if (bundle?.getString("msg") != null) {
+                val content = bundle.getString("msg")
+                showLog(content);
+            }
+        }
+    }
+
+    private fun getToken() {
+        showLog("getToken:begin")
+        object : Thread() {
+            override fun run() {
+                try {
+                    // read from agconnect-services.json
+                    val appId = "Please enter your App_Id from agconnect-services.json "
+                    val token = HmsInstanceId.getInstance(this@DaozhaoActivity).getToken(appId, "HCM")
+                    Log.i(TAG, "get token:$token")
+                    if (!TextUtils.isEmpty(token)) {
+                        sendRegTokenToServer(token)
+                    }
+                    showLog("get token:$token")
+                } catch (e: ApiException) {
+                    Log.e(TAG, "get token failed, $e")
+                    showLog("get token failed, $e")
+                }
+            }
+        }.start()
+    }
+
+
+    private fun sendRegTokenToServer(token: String?) {
+        Log.i(TAG, "sending token to server. token:$token")
+    }
+
+
+    fun showLog(text: String?) {
+        Toast.makeText(this, "aaa", Toast.LENGTH_LONG).show();
+    }
+
+    companion object {
+        private const val TAG: String = "PushDemoLog"
+        private const val GET_AAID = 1
+        private const val DELETE_AAID = 2
+        private const val CODELABS_ACTION: String = "com.daozhao.news.action"
+    }
+}
---
+++ app/src/main/java/com/daozhao/news/ui/discovery/DiscoveryViewModel.kt
@@ -0,0 +1,13 @@
+package com.daozhao.news.ui.discovery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DiscoveryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is discovery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
