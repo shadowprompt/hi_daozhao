79a5cf83250210842f15e66466105f1a2e4d5228
shadowprompt <huixiong.cn@gmail.com>
2022-03-22 16:20:27 +0800

    [fix]业务逻辑调整至fragment

--- app/src/main/java/com/daozhao/hello/DaozhaoActivity.kt
+++ app/src/main/java/com/daozhao/hello/DaozhaoActivity.kt
@@ -1,16 +1,6 @@
 package com.daozhao.hello
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.Uri
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -18,26 +8,12 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.daozhao.hello.databinding.ActivityDaozhaoBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.FirebaseApp
-import com.google.firebase.installations.FirebaseInstallations
-import com.huawei.hms.aaid.HmsInstanceId
-import com.huawei.hms.common.ApiException
-import com.huawei.hms.push.HmsMessaging
-import com.huawei.hms.push.HmsProfile
-import com.huawei.hms.push.HmsProfile.CUSTOM_PROFILE
-import okhttp3.*
-import java.io.IOException
 
 
-class DaozhaoActivity : AppCompatActivity(), View.OnClickListener {
+class DaozhaoActivity : AppCompatActivity() {
 
-    private var receiver: MyReceiver? = null
 
     private lateinit var binding: ActivityDaozhaoBinding
-
-    private var status: Boolean = false;
-
-    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,200 +33,6 @@ class DaozhaoActivity : AppCompatActivity(), View.OnClickListener {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-
-        receiver = MyReceiver()
-        val filter = IntentFilter()
-        filter.addAction(CODELABS_ACTION)
-        registerReceiver(receiver, filter)
-
-
-        findViewById<Button>(R.id.getTokenBtn).setOnClickListener(this)
-        findViewById<Button>(R.id.toggle).setOnClickListener(this)
-        findViewById<Button>(R.id.btn_action).setOnClickListener(this)
-        findViewById<Button>(R.id.btn_generate_intent).setOnClickListener(this)
-
-        FirebaseApp.initializeApp(this);
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(receiver)
-    }
-
-    inner class MyReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val bundle = intent?.extras
-            if (bundle?.getString("msg") != null) {
-                val content = bundle.getString("msg")
-                showLog(content);
-            }
-        }
-    }
-
-    private fun getToken() {
-        showLog("getToken:begin")
-        object : Thread() {
-            override fun run() {
-                try {
-                    // read from agconnect-services.json
-                    val appId = "102930575"
-
-                    val token = HmsInstanceId.getInstance(this@DaozhaoActivity).getToken(appId, "HCM")
-                    storeTokenProfile(token)
-                } catch (e: ApiException) {
-                    Log.e(TAG, "get token failed, $e")
-                    showLog("get token failed, $e")
-                }
-            }
-        }.start()
-    }
-
-    private fun storeTokenProfile(token: String?) {
-        Log.i(TAG, "get token:$token")
-        showLog("get token:$token")
-        if (!TextUtils.isEmpty(token)) {
-            sendRegTokenToServer(token);
-            // 添加当前设备上的用户与应用的关系。
-            val hmsProfileInstance: HmsProfile = HmsProfile.getInstance(this@DaozhaoActivity);
-            if (hmsProfileInstance.isSupportProfile) {
-                hmsProfileInstance.addProfile(CUSTOM_PROFILE, "9105385871708200535")
-                    .addOnCompleteListener { task ->
-                        // 获取结果
-                        if (task.isSuccessful){
-                            Log.i(TAG, "add profile success.")
-                        } else{
-                            Log.e(TAG, "add profile failed: " + task.exception.message)
-                        }
-                    }
-            }
-        }
-    }
-
-    private fun setReceiveNotifyMsg(enable: Boolean) {
-        showLog("Control the display of notification messages:begin")
-        if (enable) {
-            HmsMessaging.getInstance(this).turnOnPush().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    showLog("turnOnPush Complete")
-                    status = false;
-                } else {
-                    showLog("turnOnPush failed: cause=" + task.exception.message)
-                }
-            }
-        } else {
-            HmsMessaging.getInstance(this).turnOffPush().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    showLog("turnOffPush Complete")
-                    status = true;
-                } else {
-                    showLog("turnOffPush  failed: cause =" + task.exception.message)
-                }
-            }
-        }
-    }
-
-    /**
-     * In Opening a Specified Page of an App, how to Generate Intent parameters.
-     */
-    private fun generateIntentUri() {
-        val intent = Intent(Intent.ACTION_VIEW)
-
-        // You can add parameters in either of the following ways:
-        // Define a scheme protocol, for example, pushscheme://com.huawei.codelabpush/deeplink?.
-        // way 1 start: Use ampersands (&) to separate key-value pairs. The following is an example:
-        intent.data = Uri.parse("pushscheme://com.huawei.codelabpush/deeplink?name=abc&age=180")
-        // way 1 end. In this example, name=abc and age=180 are two key-value pairs separated by an ampersand (&).
-
-        // way 2 start: Directly add parameters to the Intent.
-        // intent.setData(Uri.parse("pushscheme://com.huawei.codelabpush/deeplink?"));
-        // intent.putExtra("name", "abc");
-        // intent.putExtra("age", 180);
-        // way 2 end.
-
-        // The following flag is mandatory. If it is not added, duplicate messages may be displayed.
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val intentUri = intent.toUri(Intent.URI_INTENT_SCHEME)
-        // The value of intentUri will be assigned to the intent parameter in the message to be sent.
-        Log.d("intentUri", intentUri)
-        showLog(intentUri)
-
-        // You can start the deep link activity with the following code.
-        intent.setClass(this, DeeplinkActivity::class.java);
-        startActivity(intent);
-    }
-
-    /**
-     * Simulate pulling up the application custom page by action.
-     */
-    private fun openActivityByAction() {
-        val intent = Intent("com.huawei.codelabpush.intent.action.test")
-
-        // You can start the deep link activity with the following code.
-        intent.setClass(this, Deeplink2Activity::class.java)
-        startActivity(intent)
-    }
-
-
-    private fun sendRegTokenToServer(token: String?) {
-        Log.i(TAG, "sending token to server. token:$token")
-        FirebaseInstallations.getInstance().id.addOnCompleteListener { it ->
-            run {
-                if (it.isComplete) {
-                    var uuid = it.result.toString();
-                    showLog("uuid complete " + uuid);
-
-                    var formBody: FormBody.Builder = FormBody.Builder();
-                    formBody.add("id", uuid);
-                    formBody.add("pushToken",token);
-                    val request: Request = Request.Builder()
-                        .url("https://gateway.daozhao.com.cn/HMS/storePushToken")
-                        .post(formBody.build())
-                        .build()
-                    val call: Call = client.newCall(request)
-                    call.enqueue(object : Callback {
-                        override fun onFailure(call: Call?, e: IOException?) {
-                            showLog("fetch failed " + uuid)
-                            Log.e(TAG, "fetch failed " + uuid);
-                        }
-
-                        @Throws(IOException::class)
-                        override fun onResponse(call: Call?, response: Response) {
-                            val result: String = response.body().string()
-                            showLog("fetch success " + uuid)
-                            Log.i(TAG, "fetch success " + uuid);
-                        }
-                    })
-                }
-            }
-        }
-
-    }
-
-
-    fun showLog(log: String?) {
-        runOnUiThread {
-            val textView = findViewById<View?>(R.id.msgText)
-            if (textView is TextView) {
-                textView.text = log
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG: String = "PushDemoLog"
-        private const val GET_AAID = 1
-        private const val DELETE_AAID = 2
-        private const val CODELABS_ACTION: String = "com.daozhao.hello.action"
-    }
-
-    override fun onClick(v: View?) {
-        when(v?.id) {
-            R.id.getTokenBtn -> getToken()
-            R.id.toggle -> setReceiveNotifyMsg(status)
-            R.id.btn_action -> openActivityByAction()
-            R.id.btn_generate_intent -> generateIntentUri()
-        }
     }
 
 }
--- app/src/main/java/com/daozhao/hello/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/daozhao/hello/ui/notifications/NotificationsFragment.kt
@@ -1,10 +1,12 @@
 package com.daozhao.hello.ui.notifications
 
 import android.R
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -14,15 +16,25 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.daozhao.hello.DaozhaoActivity
+import com.daozhao.hello.Deeplink2Activity
+import com.daozhao.hello.DeeplinkActivity
 import com.daozhao.hello.databinding.FragmentNotificationsBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.installations.FirebaseInstallations
 import com.huawei.hms.aaid.HmsInstanceId
 import com.huawei.hms.common.ApiException
+import com.huawei.hms.push.HmsMessaging
+import com.huawei.hms.push.HmsProfile
+import okhttp3.*
+import java.io.IOException
 
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment() , View.OnClickListener {
 
 
     private lateinit var notificationsViewModel: NotificationsViewModel
@@ -32,27 +44,60 @@ class NotificationsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-//    private var receiver: MyReceiver? = null
+    private var root: View? = null;
+
+    private var status: Boolean = false;
+
+    private val client = OkHttpClient()
+
+    private var receiver: MyReceiver? = null
+
+    private var mContext: Context ?= null
 
     private var btn: Button? = null;
 
+    inner class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val bundle = intent?.extras
+            if (bundle?.getString("msg") != null) {
+                val content = bundle.getString("msg")
+                showLog(content);
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context;
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+
+        root = binding.root
 
         val textView: TextView = binding.textNotifications
+
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
-        btn = binding.getTokenBtn;
+
+//
+//       root.findViewById<Button>(com.daozhao.hello.R.id.btnPt).setOnClickListener(this)
+        (root as ConstraintLayout).findViewById<Button>(com.daozhao.hello.R.id.getTokenBtn2).setOnClickListener(this)
+        (root as ConstraintLayout).findViewById<Button>(com.daozhao.hello.R.id.toggle2).setOnClickListener(this)
+        (root as ConstraintLayout).findViewById<Button>(com.daozhao.hello.R.id.btn_action2).setOnClickListener(this)
+        (root as ConstraintLayout).findViewById<Button>(com.daozhao.hello.R.id.btn_generate_intent2).setOnClickListener(this)
+        (root as ConstraintLayout).findViewById<Button>(com.daozhao.hello.R.id.btn_web2).setOnClickListener(this)
+
+        FirebaseApp.initializeApp(mContext!!)
+
         return root
     }
 
@@ -61,80 +106,188 @@ class NotificationsFragment : Fragment() {
         _binding = null
     }
 
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//
-//        btn!!.setOnClickListener(object : View.OnClickListener() {
-//            override fun onClick(v: View?) {
-//                // TODO Auto-generated method stub
-//                Toast.makeText(activity, "success2", Toast.LENGTH_LONG).show()
-//            }
-//        })
-//    }
+    private fun getToken() {
+        showLog("getToken:begin")
+        object : Thread() {
+            override fun run() {
+                try {
+                    // read from agconnect-services.json
+                    val appId = "102930575"
 
-    fun clickFun(v: View) {
-        Toast.makeText(activity, "success2", Toast.LENGTH_LONG).show()
+                    val token = HmsInstanceId.getInstance(mContext).getToken(appId, "HCM")
+                    storeTokenProfile(token)
+                } catch (e: ApiException) {
+                    Log.e(TAG, "get token failed, $e")
+//                    showLog("get token failed, $e")
+                }
+            }
+        }.start()
     }
 
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        receiver = MyReceiver()
-//        val filter = IntentFilter()
-//        filter.addAction(CODELABS_ACTION)
-////        registerReceiver(receiver, filter)
-//    }
-//
-
-//
-//    private fun getToken() {
-//        showLog("getToken:begin")
-//        object : Thread() {
-//            override fun run() {
-//                try {
-//                    // read from agconnect-services.json
-////                    val appId = "102930575"
-//                    val appId = "Please enter your App_Id from agconnect-services.json "
-//
-//                    val token = HmsInstanceId.getInstance(this@DaozhaoActivity).getToken(appId, "HCM")
-//                    Log.i(TAG, "get token:$token")
-//                    if (!TextUtils.isEmpty(token)) {
-//                        sendRegTokenToServer(token)
+    private fun storeTokenProfile(token: String?) {
+        Log.i(TAG, "get token:$token")
+        showLog("get token:$token")
+//        if (!TextUtils.isEmpty(token)) {
+//            sendRegTokenToServer(token);
+//            // 添加当前设备上的用户与应用的关系。
+//            val hmsProfileInstance: HmsProfile = HmsProfile.getInstance(mContext);
+//            if (hmsProfileInstance.isSupportProfile) {
+//                hmsProfileInstance.addProfile(HmsProfile.CUSTOM_PROFILE, "9105385871708200535")
+//                    .addOnCompleteListener { task ->
+//                        // 获取结果
+//                        if (task.isSuccessful){
+//                            Log.i(TAG, "add profile success.")
+//                        } else{
+//                            Log.e(TAG, "add profile failed: " + task.exception.message)
+//                        }
 //                    }
-//                    showLog("get token:$token")
-//                } catch (e: ApiException) {
-//                    Log.e(TAG, "get token failed, $e")
-//                    showLog("get token failed, $e")
-//                }
-//            }
-//        }.start()
-//    }
-//
-//
-//    private fun sendRegTokenToServer(token: String?) {
-//        Log.i(TAG, "sending token to server. token:$token")
-//    }
-//
-//
-//    fun showLog(text: String?) {
-////        Toast.makeText(this, "aaa", Toast.LENGTH_LONG).show();
-//    }
-//
-//    inner class MyReceiver : BroadcastReceiver() {
-//        override fun onReceive(context: Context?, intent: Intent?) {
-//            val bundle = intent?.extras
-//            if (bundle?.getString("msg") != null) {
-//                val content = bundle.getString("msg")
-//                showLog(content);
 //            }
 //        }
-//    }
-//
-//    companion object {
-//        private const val TAG: String = "PushDemoLog"
-//        private const val GET_AAID = 1
-//        private const val DELETE_AAID = 2
-//        private const val CODELABS_ACTION: String = "com.daozhao.hello.action"
-//    }
+    }
+
+    private fun setReceiveNotifyMsg(enable: Boolean) {
+        showLog("Control the display of notification messages:begin")
+        if (enable) {
+            HmsMessaging.getInstance(mContext).turnOnPush().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    showLog("turnOnPush Complete")
+                    status = false;
+                } else {
+                    showLog("turnOnPush failed: cause=" + task.exception.message)
+                }
+            }
+        } else {
+            HmsMessaging.getInstance(mContext).turnOffPush().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    showLog("turnOffPush Complete")
+                    status = true;
+                } else {
+                    showLog("turnOffPush  failed: cause =" + task.exception.message)
+                }
+            }
+        }
+    }
+
+    /**
+     * In Opening a Specified Page of an App, how to Generate Intent parameters.
+     */
+    private fun generateIntentUri() {
+        val intent = Intent(Intent.ACTION_VIEW)
+
+        // You can add parameters in either of the following ways:
+        // Define a scheme protocol, for example, pushscheme://com.huawei.codelabpush/deeplink?.
+        // way 1 start: Use ampersands (&) to separate key-value pairs. The following is an example:
+        intent.data = Uri.parse("pushscheme://com.huawei.codelabpush/deeplink?name=abc&age=180")
+        // way 1 end. In this example, name=abc and age=180 are two key-value pairs separated by an ampersand (&).
+
+        // way 2 start: Directly add parameters to the Intent.
+        // intent.setData(Uri.parse("pushscheme://com.huawei.codelabpush/deeplink?"));
+        // intent.putExtra("name", "abc");
+        // intent.putExtra("age", 180);
+        // way 2 end.
+
+        // The following flag is mandatory. If it is not added, duplicate messages may be displayed.
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val intentUri = intent.toUri(Intent.URI_INTENT_SCHEME)
+        // The value of intentUri will be assigned to the intent parameter in the message to be sent.
+        Log.d("intentUri", intentUri)
+        showLog(intentUri)
+
+        // You can start the deep link activity with the following code.
+        intent.setClass(mContext!!, DeeplinkActivity::class.java);
+        startActivity(intent);
+    }
+
+    /**
+     * Simulate pulling up the application custom page by action.
+     */
+    private fun openActivityByAction() {
+        val intent = Intent("com.huawei.codelabpush.intent.action.test")
+
+        // You can start the deep link activity with the following code.
+        intent.setClass(mContext!!, Deeplink2Activity::class.java)
+        startActivity(intent)
+    }
+
+
+    private fun sendRegTokenToServer(token: String?) {
+        Log.i(TAG, "sending token to server. token:$token")
+        FirebaseInstallations.getInstance().id.addOnCompleteListener { it ->
+            run {
+                if (it.isComplete) {
+                    var uuid = it.result.toString();
+                    showLog("uuid complete " + uuid);
+
+                    var formBody: FormBody.Builder = FormBody.Builder();
+                    formBody.add("id", uuid);
+                    formBody.add("pushToken",token);
+                    val request: Request = Request.Builder()
+                        .url("https://gateway.daozhao.com.cn/HMS/storePushToken")
+                        .post(formBody.build())
+                        .build()
+                    val call: Call = client.newCall(request)
+                    call.enqueue(object : Callback {
+                        override fun onFailure(call: Call?, e: IOException?) {
+                            showLog("fetch failed " + uuid)
+                            Log.e(TAG, "fetch failed " + uuid);
+                        }
+
+                        @Throws(IOException::class)
+                        override fun onResponse(call: Call?, response: Response) {
+                            val result: String = response.body().string()
+                            showLog("fetch success " + uuid)
+                            Log.i(TAG, "fetch success " + uuid);
+                        }
+                    })
+                }
+            }
+        }
+
+    }
+
+
+    fun showLog(log: String?) {
+//        runOnUiThread {
+            val textView = root!!.findViewById<View?>(com.daozhao.hello.R.id.msgText2)
+            if (textView is TextView) {
+                textView.text = log
+            }
+//        }
+    }
+
+    fun openWeb() {
+        val webpage: Uri = Uri.parse("https://www.daozhao.com")
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(mContext!!.packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        private const val TAG: String = "PushDemoLog"
+        private const val GET_AAID = 1
+        private const val DELETE_AAID = 2
+        private const val CODELABS_ACTION: String = "com.daozhao.hello.action"
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            com.daozhao.hello.R.id.getTokenBtn2 -> getToken()
+            com.daozhao.hello.R.id.toggle2 -> setReceiveNotifyMsg(status)
+            com.daozhao.hello.R.id.btn_action2 -> openActivityByAction()
+            com.daozhao.hello.R.id.btn_generate_intent2 -> generateIntentUri()
+            com.daozhao.hello.R.id.btn_web2 -> openWeb()
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        receiver = MyReceiver()
+        val filter = IntentFilter()
+        filter.addAction(CODELABS_ACTION)
+        mContext!!.registerReceiver(receiver, filter)
+    }
+
 }
