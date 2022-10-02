5eb8fefc5c23e60a20e587c59081a4a6d41c6e49
shadowprompt <huixiong.cn@gmail.com>
2022-10-02 22:38:11 +0800

    [fix]支持和webview的JavaScript交互

--- app/src/main/java/com/daozhao/hello/ui/home/HomeFragment.kt
+++ app/src/main/java/com/daozhao/hello/ui/home/HomeFragment.kt
@@ -1,19 +1,34 @@
 package com.daozhao.hello.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.daozhao.hello.R
 import com.daozhao.hello.UrlViewModel
 import com.daozhao.hello.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+
+class WebAppInterface(private val mContext: Context) {
+
+    // 在JS中直接调用Android的方法
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+    }
+}
+
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -22,6 +37,7 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var mContext: Context? = null;
     private var myWebView: WebView? = null
 
     private val viewModel: UrlViewModel by activityViewModels()
@@ -46,7 +62,16 @@ class HomeFragment : Fragment() {
             loadUrl(it)
         })
 
+        root.findViewById<Button>(R.id.webClick).setOnClickListener(this)
+        root.findViewById<Button>(R.id.loadLocal).setOnClickListener(this)
+        root.findViewById<Button>(R.id.loadDaozhao).setOnClickListener(this)
+
         return root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context;
     }
 
     private fun initWebview() {
@@ -64,6 +89,8 @@ class HomeFragment : Fragment() {
 //        myWebView!!.settings.builtInZoomControls = true
         // 设定缩放控件隐藏
         myWebView!!.settings.displayZoomControls = true
+
+        myWebView!!.addJavascriptInterface(WebAppInterface(mContext!!), "AndroidFunction");
     }
 
     fun loadUrl(url: String) {
@@ -74,5 +101,22 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun webClick() {
+        // 通过Handler发送消息
+        // 通过Handler发送消息
+        myWebView!!.post { // 注意调用的JS方法名要对应上
+            // 调用javascript的callJS()方法
+            myWebView!!.loadUrl("javascript:callJS()")
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.webClick -> webClick();
+            R.id.loadLocal -> loadUrl("file:///android_asset/javascript.html");
+            R.id.loadDaozhao -> loadUrl("https://www.daozhao.com");
+        }
     }
 }
